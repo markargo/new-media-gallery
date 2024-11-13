ba36a7ab84c7449b7ac979f2f5248ab48fbee66f
Mark Argo <mark@littlerobotfriends.com>
2024-11-13 03:39:58 -0500

    added some basics for the exhibition page

--- react-app/src/constants.ts
+++ react-app/src/constants.ts
@@ -1,1 +1,162 @@
+
+export interface Artist {
+  id: string;
+  name: string;
+}
+
+export interface Exhibition {
+  id: string;
+  name: string;
+  img: string;
+  desc: string;
+  footer: string;
+  startDate: string;
+  endDate: string;
+  artists: Artist[];
+  mediaGallery: string[];
+}
+
+export interface Project {
+  id: string;
+  name: string;
+  desc: string;
+  startDate: string;
+  endDate: string;
+  artists: Artist[];
+  mediaGallery: string[];
+}
+
+export const PLACEHOLDER_EXHIBITIONS: Exhibition[] = [
+  {
+    id: 'Fragments2024',
+    name: 'Fragments (NM24)',
+    img: 'https://placeholder.co/600x600',
+    desc: 'Fragments showcases the work of artists who explore the concept of fragmentation in their work.',
+    footer: 'The Fragments exhibition ran from April 17th 2024 to April 23rd 2024.',
+    startDate: '2024-04-17',
+    endDate: '2024-04-23',
+    artists: [
+      {
+        id: 'Artist1',
+        name: 'Artist 1',
+      },
+      {
+        id: 'Artist2',
+        name: 'Artist 2',
+      },
+      {
+        id: 'Artist3',
+        name: 'Artist 3',
+      },
+    ],
+    mediaGallery: [
+      'https://placeholder.co/150x150',
+      'https://placeholder.co/150x150',
+      'https://placeholder.co/150x150',
+      'https://placeholder.co/150x150',
+      'https://placeholder.co/150x150',
+    ],
+  },
+  {
+    id: 'Exhibition2',
+    name: 'Exhibition 2',
+    img: 'https://placeholder.co/600x600',
+    desc: 'Description for Exhibition 2',
+    footer: 'The Exhibition 2 ran from April 24th 2024 to April 30th 2024.',
+    startDate: '2024-04-24',
+    endDate: '2024-04-30',
+    artists: [
+      {
+        id: 'Artist4',
+        name: 'Artist 4',
+      },
+      {
+        id: 'Artist5',
+        name: 'Artist 5',
+      },
+      {
+        id: 'Artist6',
+        name: 'Artist 6',
+      },
+    ],
+    mediaGallery: [
+      'https://via.placeholder.com/150',
+      'https://via.placeholder.com/150',
+      'https://via.placeholder.com/150',
+    ],
+  },
+  {
+    id: 'Exhibition3',
+    name: 'Exhibition 3',
+    img: 'https://placeholder.co/600x600',
+    desc: 'Description for Exhibition 3',
+    footer: 'The Exhibition 3 ran from May 1st 2024 to May 7th 2024.',
+    startDate: '2024-05-01',
+    endDate: '2024-05-07',
+    artists: [
+      {
+        id: 'Artist7',
+        name: 'Artist 7',
+      },
+      {
+        id: 'Artist8',
+        name: 'Artist 8',
+      },
+      {
+        id: 'Artist9',
+        name: 'Artist 9',
+      },
+    ],
+    mediaGallery: [
+      'https://placeholder.com/150',
+      'https://via.placeholder.com/150',
+      'https://via.placeholder.com/150',
+    ],
+  },
+]
+
+export const PLACEHOLDER_ARTISTS: Artist[] = [
+  {
+    id: 'Artist1',
+    name: 'Artist 1',
+  },
+  {
+    id: 'Artist2',
+    name: 'Artist 2',
+  },
+  {
+    id: 'Artist3',
+    name: 'Artist 3',
+  },
+  {
+    id: 'Artist4',
+    name: 'Artist 4',
+  },
+  {
+    id: 'Artist5',
+    name: 'Artist 5',
+  },
+  {
+    id: 'Artist6',
+    name: 'Artist 6',
+  },
+  {
+    id: 'Artist7',
+    name: 'Artist 7',
+  },
+  {
+    id: 'Artist8',
+    name: 'Artist 8',
+  },
+  {
+    id: 'Artist9',
+    name: 'Artist 9',
+  },
+  {
+    id: 'Artist10',
+    name: 'Artist 10',
+  },
+]
+
+
 export {}
--- react-app/src/pages/ExhibitionPage.tsx
+++ react-app/src/pages/ExhibitionPage.tsx
@@ -1,21 +1,99 @@
 import React from 'react';
 import './ExhibitionPage.scss';
+import { Artist, PLACEHOLDER_ARTISTS, PLACEHOLDER_EXHIBITIONS } from '../constants';
 
 interface ExhibitionPageProps {
   // Define your component's props here
 }
 
 const ExhibitionPage: React.FC<ExhibitionPageProps> = () => {
+
+  const [headerImage, setHeaderImage] = React.useState<string>(PLACEHOLDER_EXHIBITIONS[0].img);
+  const [headerTitle, setHeaderTitle] = React.useState<string>(PLACEHOLDER_EXHIBITIONS[0].name);
+  const [headerDesc, setHeaderDesc] = React.useState<string>(PLACEHOLDER_EXHIBITIONS[0].desc);
+  const [featuredArtists, setFeaturedArtists] = React.useState<Artist[]>(PLACEHOLDER_ARTISTS);
+  const [footerDesc, setFooterDesc] = React.useState<string>(PLACEHOLDER_EXHIBITIONS[0].footer);
+  const [mediaGallery, setMediaGallery] = React.useState<string[]>(PLACEHOLDER_EXHIBITIONS[0].mediaGallery);
+
+  const renderHeaderImage = () => {
+    return (
+      <div className='header-image'>
+        <img src={ headerImage } alt='Exhibition header' />
+      </div>
+    );
+  }
+
+  const renderHeaderTitle = () => {
+    return (
+      <div className='header-title'>
+        { headerTitle }
+      </div>
+    );
+  }
+
+  const renderHeaderDesc = () => {
+    return (
+      <div className='header-desc'>
+        { headerDesc }
+      </div>
+    );
+  }
+
+  const renderFeaturedArtists = () => {
+    return (
+      <div className='featured-artists'>
+        Featuring work by: { 
+          featuredArtists.map((artist, index) => {
+            return (
+              <span key={ artist.id }>
+                <a href={`/artist/${artist.id}`}>{ artist.name }</a>{ index < featuredArtists.length - 1 ? ', ' : '' }
+              </span>
+            );
+          }
+        )}
+      </div>
+    );
+  }
+
+  const renderFooterDesc = () => {
+    return (
+      <div className='footer-desc'>
+        { footerDesc }
+      </div>
+    );
+  }
+
+  const renderMediaGallery = () => {
+    return (
+      <div className='media-gallery-wrapper'>
+        <div>
+          Media Gallery:
+        </div>
+        <div className='media-gallery'>
+          {
+            mediaGallery.map((media, index) => {
+              return (
+                <img key={ index } src={ media } alt='Media gallery item' />
+              );
+            })
+          }
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className='exhibition-page-wrapper'>
       <h1>Exhibition Page</h1>
-      {/* renderHeaderImage() */}
-      {/* renderHeaderTitle() */}
-      {/* renderHeaderDesc() */}
-      {/* renderFeaturedArtists() */}
-      {/* renderFooterDesc() */}
-      {/* renderMediaGallery() */}
+      <div className='page-container'>
+        {renderHeaderImage()}
+        {renderHeaderTitle()}
+        {renderHeaderDesc()}
+        {renderFeaturedArtists()}
+        {renderFooterDesc()}
+        {renderMediaGallery()}
       </div>
+    </div>
   );
 };
 
