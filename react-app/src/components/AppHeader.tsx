5d98a8b1e0c4f49701896efa0155b9df15e425b1
Mark Argo <mark@littlerobotfriends.com>
2025-04-14 18:18:22 -0400

    Header tweak for metamorphosis

--- react-app/src/components/AppHeader.tsx
+++ react-app/src/components/AppHeader.tsx
@@ -12,7 +12,10 @@ const AppHeader: React.FC<AppHeaderProps> = () => {
   return (
     <div className='app-header-wrapper'>
       <div className='app-header-title'>
-        <Link to='/'>new media<span className='title-accent'>@tmu</span></Link>
+        {/* <Link to='/'>new media<span className='title-accent'>@tmu</span></Link> */}
+        <Link to='/'>
+          <img src={'/media/METAMORPHOSIS-Main-Logo.png'} alt='Logo' />
+        </Link>
       </div>
       <div className='app-header-nav'>
         <Link to='/exhibition'>Exhibitions</Link>&nbsp;|&nbsp;
