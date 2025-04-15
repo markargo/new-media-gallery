7ec1bb4013856dd53a00dd6e7634bd458b43841b
Mark Argo <mark@littlerobotfriends.com>
2025-04-14 20:44:38 -0400

    auto-routing to exhibition page

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
 import './App.scss';
 
 import HomePage from './pages/HomePage';
@@ -15,13 +15,14 @@ function App() {
       <div className='app-wrapper'>
         <AppHeader />
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          {/* <Route path="/" element={<HomePage />} /> */}
           <Route path="/exhibition/" element={<ExhibitionPage />} />
           <Route path="/exhibition/:id" element={<ExhibitionPage />} />
           <Route path="/artist/" element={<ArtistPage />} />
           <Route path="/artist/:id" element={<ArtistPage />} />
           <Route path="/project/" element={<ProjectPage />} />
           <Route path="/project/:id" element={<ProjectPage />} />
+          <Route path="/" element={<Navigate to={"/exhibition/metamorphosis"} replace />}/>
         </Routes>
         {/* <AppFooter /> */}
       </div>
