816e9aac28382969d3f15a06c80694da102c1e43
Mark Argo <mark@aesthetec.net>
2026-09-19 18:32:39 -0400

    Truncating long project titles

--- react-app/src/components/ProjectItem.tsx
+++ react-app/src/components/ProjectItem.tsx
@@ -5,10 +5,29 @@ import { Link } from 'react-router-dom';
 
 interface ProjectItemProps {
   isLink?: boolean;
+  maxNameLength?: number;
   project: Project;
 }
 
-const ProjectItem: React.FC<ProjectItemProps> = ({ project, isLink=false, }) => {
+const DEFAULT_MAX_NAME_LENGTH = 32;
+
+const truncateName = (name: string, maxLength: number) => {
+  if (name.length <= maxLength) {
+    return name;
+  }
+
+  if (maxLength <= 3) {
+    return name.slice(0, maxLength);
+  }
+
+  return `${name.slice(0, maxLength - 3)}...`;
+};
+
+const ProjectItem: React.FC<ProjectItemProps> = ({
+  project,
+  isLink = false,
+  maxNameLength = DEFAULT_MAX_NAME_LENGTH,
+}) => {
 
   const renderContents = () => {
     return (
@@ -16,7 +35,7 @@ const ProjectItem: React.FC<ProjectItemProps> = ({ project, isLink=false, }) => 
         {/* <img src={ project.img } alt='Project' /> */}
         <img src={ '/' + project.img } onError={(e) => { e.currentTarget.onerror = null; e.currentTarget.src = PLACEHOLDER_IMAGE_LG; }} alt={project.name}></img>
         <div className='project-item-title'>
-          { project.name }
+          { truncateName(project.name, maxNameLength) }
         </div>
       </div>
     )
